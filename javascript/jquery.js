6709e580d650c7c13fe64632bcca3881a05b9b80
Aadhil Abdulla <aadhil242@gmail.com>
2024-09-24 12:01:05 +0530

    JQuery moved to new file

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,47 +1,59 @@
-$(function(){
-    $("#submitButton").click(function(){
-        if ($("#name").val() == ""){
-            $("#namewarning").show();
-        }
-        else{
-            $("#namewarning").hide();
-        }
+function validation(){
+    let validate = true
+    $(function(){
+            if ($("#name").val() == ""){
+                $("#namewarning").show();
+                validate = false
+            }
+            else{
+                $("#namewarning").hide();
+            }
+            
+            
+            if ($("#email").val() == ""){
+                $("#emailwarning").show();
+                validate = false
+            }
+            else{
+                $("#emailwarning").hide();
+            }
+            
+            
+            if ($("#password").val() == ""){
+                $("#passwordwarning").show();
+                validate = false
+            }
+            else{
+                $("#passwordwarning").hide();
+            }
+            
+            
+            if ($("#textarea").val() == ""){
+                $("#textareawarning").show();
+                validate = false
+            }
+            else{
+                $("#textareawarning").hide();
+            }
+            
+            if ($('input[name="gender"]:checked').length == 0) {
+                $("#genderwarning").show();  // Show warning if no radio button is selected
+                validate = false
+            } else {
+                $("#genderwarning").hide();  // Hide warning if a radio button is selected
+            }
+            
+            if ($('input[name="hobby"]:checked').length == 0) {
+                $("#checkboxwarning").show();  // Show warning if no radio button is selected
+                validate = false
+            } else {
+                $("#checkboxwarning").hide();  // Hide warning if a radio button is selected
+            }
 
-
-        if ($("#email").val() == ""){
-            $("#emailwarning").show();
-        }
-        else{
-            $("#emailwarning").hide();
-        }
-
-
-        if ($("#password").val() == ""){
-            $("#passwordwarning").show();
-        }
-        else{
-            $("#passwordwarning").hide();
-        }
-        
-        
-        if ($("#textarea").val() == ""){
-            $("#textareawarning").show();
-        }
-        else{
-            $("#textareawarning").hide();
-        }
-
-        if ($('input[name="gender"]:checked').length == 0) {
-            $("#genderwarning").show();  // Show warning if no radio button is selected
-        } else {
-            $("#genderwarning").hide();  // Hide warning if a radio button is selected
-        }
-
-        if ($('input[name="hobby"]:checked').length == 0) {
-            $("#checkboxwarning").show();  // Show warning if no radio button is selected
-        } else {
-            $("#checkboxwarning").hide();  // Hide warning if a radio button is selected
-        }
-
-    });
-});
+            if(validate){
+                $("#form")[0].submit($(function(){
+                    alert("Form is submitted")
+                }));
+            }
+        });
+}
